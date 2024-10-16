8c6a538d52ea7b0880594e236b80ca00849418c0
SaikrishnanPerumal <sai.krishnan663@gmail.com>
2024-10-16 23:46:04 +0530

    setup context infra
    Signed-off-by: SaikrishnanPerumal <sai.krishnan663@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import './App.css'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import DashBoard from './Pages/DashBoard'
+import { CitiesContextProvider } from './Hooks/useCitesContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 60 * 1000
+    }
+  }
+});
 
 function App() {
-
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path='/dashboard' element={<DashBoard />}/>
-          <Route path='/' element={<h3>This route is not defined</h3>}/>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <CitiesContextProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Navigate replace to="dashboard" />} />
+              <Route path='/dashboard' element={<DashBoard />}/>
+              <Route path='/' element={<h3>This route is not defined</h3>}/>
+            </Routes>
+          </BrowserRouter>
+          </CitiesContextProvider>
+        </QueryClientProvider>
     </>
   )
 }
--- src/Pages/DashBoard.tsx
+++ src/Pages/DashBoard.tsx
@@ -1,9 +1,13 @@
+import BottomSectionComponent from '../Components/BottomSectionComponent/BottomSectionComponent';
 import CurrentCityComponent from '../Components/CurrentCitySectionComponent/CurrentCitySectionComponent';
+import MiddleSectionComponent from '../Components/MiddleSectionComponent/MiddleSectionComponent';
 import styles from './DashBoard.module.scss';
 
 const DashBoard = () => {
     return <div className={styles.dashboardContainer}>
         <CurrentCityComponent />
+        <MiddleSectionComponent />
+        <BottomSectionComponent />
     </div>;
 }
  
