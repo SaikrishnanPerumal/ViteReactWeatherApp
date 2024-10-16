cd044bfbbffb851dd45ec5ddc0d6faaa83af6b44
SaikrishnanPerumal <sai.krishnan663@gmail.com>
2024-10-16 23:46:51 +0530

    feat: add functionality for city selection section
    Signed-off-by: SaikrishnanPerumal <sai.krishnan663@gmail.com>

---
+++ src/Components/SelectedCityForecast/SelectedCityForecast.tsx
@@ -0,0 +1,77 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useCitiesContext } from '../../Hooks/useCitesContext';
+import { CityWeatherForecast, getCityDateTimeApi, getCityWeatherForecastApi } from '../../Services/cityWeatherApis';
+import styles from './SelectedCityForecast.module.scss';
+
+const getTimeListForForecast = (city_Date_Time_Name: string, dummyForecastData: CityWeatherForecast) => {
+    const timeList: string[] = ['NOW'];
+    const time = city_Date_Time_Name.split(', ')[1];
+    const timeAndAmPm = time.split(' ');
+    let hour = +timeAndAmPm[0].split(':')[0];
+    let period = timeAndAmPm[1];
+
+    for (let i = 1; i < dummyForecastData.hours.length; i++) {
+        hour++;
+        if (hour === 12) {
+            period = period === 'AM' ? 'PM' : 'AM';
+        }
+        timeList.push(`${hour === 12 ? 12 : hour % 12} ${period}`);
+    }
+
+    return timeList;
+}
+
+const getWeatherIconBasedOnTemperature = (temp: number): string => {
+    if (temp < 10) {
+        return 'public/weather-icons/humidityIcon.svg';
+    } else if (temp >= 10 && temp < 20) {
+        return 'public/weather-icons/cloudyIcon.svg';
+    } else {
+        return 'public/weather-icons/sunnyIcon.svg';
+    }
+}
+
+const SelectedCityForecast = () => {
+    const { selectedCity } = useCitiesContext();
+    const queryClient = useQueryClient();
+
+    const {data: selectedCityDateTime} = useQuery(['selectCityDateTime', selectedCity], {
+        queryFn: () => getCityDateTimeApi(selectedCity),
+        onSuccess: () => {
+            queryClient.invalidateQueries(['selectCityForecast']);
+        },
+        refetchOnMount: true
+    });
+
+    const {data: selectedCityForecast} = useQuery(['selectCityForecast', selectedCityDateTime], {
+        queryFn: () => {
+            if (selectedCityDateTime) {
+                return getCityWeatherForecastApi(selectedCityDateTime, 5);
+            }
+            return Promise.reject(undefined);
+        }
+    });
+
+    if(!selectedCityDateTime || !selectedCityForecast) {
+        return null;
+    }
+
+    const timeList = getTimeListForForecast(selectedCityDateTime.city_Date_Time_Name, selectedCityForecast);
+
+    return <>
+    <div className={styles.forecastContainer}>
+    { selectedCityForecast ? selectedCityForecast.temperature.map((temp, index) => {
+        return <div key={index} className={styles.forecastFragContainer}>
+            <span className={styles.forecastTime}>{timeList[index]}</span>
+            <div className={styles.lineBreak}></div>
+            <img className={styles.WeatherIcon} src={getWeatherIconBasedOnTemperature(parseInt(temp))}></img>
+            <div>{temp}</div>
+        </div>
+    })
+        : <div className={styles.forecastContainer + ' ' + styles.loadingForecast}>Loading forecast...</div>
+    }
+    </div>
+    </>;
+}
+ 
+export default SelectedCityForecast;
---
+++ src/Components/SelectedCityTime/SelectedCityTime.tsx
@@ -0,0 +1,64 @@
+import { City } from "../../Services/cityWeatherApis";
+import styles from "./SelectedCityTime.module.scss";
+
+interface CityDateAndTime {
+  date: string;
+  month: string;
+  year: string;
+  hour: string;
+  min: string;
+  sec: string;
+  amPm: string;
+}
+
+interface SelectedCityTimeProps {
+  cityDetails: City;
+}
+
+const SelectedCityTime = ({ cityDetails }: SelectedCityTimeProps) => {
+
+  const getSelectedCityDateTime = (
+    selectedCityDetails: City
+  ): CityDateAndTime => {
+      // "10/16/2024, 5:26:56 AM"
+      return selectedCityDetails.dateAndTime.split(", ").reduce(
+        (acc, dateAndTime, index) => {
+          if (index === 0) {
+            const [month, date, year] = dateAndTime.split("/");
+            return { ...acc, month, date, year };
+          } else {
+            const [time, amPm] = dateAndTime.split(" ");
+            const [hour, min, sec] = time.split(":");
+            return {
+              ...acc,
+              hour,
+              min,
+              sec,
+              amPm,
+            };
+          }
+        },
+        { date: "", month: "", year: "", hour: '', min: '', sec: '', amPm: "" }
+      );
+  };
+
+  const selectedCityDateTime = getSelectedCityDateTime(cityDetails);
+
+  return (
+    <div className={styles.selectedCityDateAndTime}>
+      <div className={styles.emptyContainer}></div>
+      <div className={styles.selectedCityTime}>
+        <span className={styles.cityTime}>{selectedCityDateTime.hour.padStart(2, '0')}</span>:
+        <span className={styles.cityTime}>{selectedCityDateTime.min.padStart(2, '0')}</span>:
+        <span className={styles.citySec}>{selectedCityDateTime.sec.padStart(2, '0')}</span>
+        <img
+          src={`public/general-icons/${selectedCityDateTime.amPm.toLowerCase()}.svg`}
+          alt="AM/PM"
+          className={styles.amPmIcon}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SelectedCityTime;
